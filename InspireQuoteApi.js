3a0be6cc53d7e401a4d337e9171e8547fb73d1bf
TOMASINI Laura <tomasinilaura1988@gmail.com>
2024-01-08 14:41:25 +0100

    add filter bu author

--- InspireQuoteApi.js
+++ InspireQuoteApi.js
@@ -10,7 +10,7 @@ export default class InspireQuoteApi {
 
     async getInspiringQuote() {
         try {
-            const response = await fetch(this.apiUrl);
+            const response = await fetch(this.apiUrl + '/quotes/random');
             if (!response.ok) {
                 throw new Error('La requête a échoué');
             }
@@ -29,5 +29,58 @@ export default class InspireQuoteApi {
         } catch (error) {
             console.error('Une erreur s\'est produite :', error);
         }
+    }
+
+    async getAuthors() {
+        try {
+            const response = await fetch(this.apiUrl + "/authors");
+            if (!response.ok) {
+                throw new Error('La requête a échoué');
+            }
+
+            const data = await response.json();
+            const authorSelect = document.getElementById('authorSelect');
+
+            // Ajoutez les auteurs en tant qu'options à l'élément <select>
+            data.results.forEach(author => {
+                const option = document.createElement('option');
+                option.value = author.slug;
+                option.textContent = author.name;
+                authorSelect.appendChild(option);
+            });
+        } catch (error) {
+            console.error('Une erreur s\'est produite :', error);
+        }
+    }
+
+
+
+    async getInspiringQuoteByAuthor(authorSlug) {
+        try {
+            const response = await fetch(this.apiUrl + "/quotes/random?author=" + encodeURIComponent(authorSlug));
+            if (!response.ok) {
+                throw new Error('La requête a échoué');
+            }
+
+            const data = await response.json();
+            console.log(data)
+            const quoteText = data[0].content;
+            const quoteAuthor = data[0].author;
+
+            // Crée une instance de la classe Msg et affiche la citation
+            try {
+                const msg = new Msg('msg', quoteText, quoteAuthor);
+                msg.render();
+            } catch (e) {
+                console.error(e);
+            }
+        } catch (error) {
+            console.error('Une erreur s\'est produite :', error);
+        }
+    }
+
+    async initialize() {
+        await this.getAuthors();
+        // Vous pouvez également appeler getInspiringQuote() pour afficher une citation initiale au chargement de la page.
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // index.js
 import InspireQuoteApi from "./InspireQuoteApi.js";
 
-const apiUrl = 'https://api.quotable.io/quotes/random';
+const apiUrl = 'https://api.quotable.io';
 const quoteFetcher = new InspireQuoteApi(apiUrl);
+quoteFetcher.initialize();
 quoteFetcher.getInspiringQuote();
 
 const button = document.getElementById('generateButton');
 button.addEventListener('click', () => {
     quoteFetcher.getInspiringQuote();
 })
+
+const authorSelect = document.getElementById('authorSelect');
+authorSelect.addEventListener('change', () => {
+    const selectedAuthorId = authorSelect.value; // Obtenez la valeur de l'auteur sélectionné
+    if (selectedAuthorId) {
+        // Si une option est sélectionnée, appelez getInspiringQuoteByAuthor avec l'ID de l'auteur
+        quoteFetcher.getInspiringQuoteByAuthor(selectedAuthorId);
+    }
+});
